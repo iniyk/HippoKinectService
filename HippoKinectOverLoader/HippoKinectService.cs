0562b36f20a4afa38c6f01351e79664ce87f2dea
iniyk <iniykfrost@gmail.com>
2014-03-11 18:55:07 +0800

    a runable service

---
+++ HippoKinectOverLoader/HippoKinectService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HippoKinectOverLoader {
+    public partial class HippoKinectService : ServiceBase {
+        public HippoKinectService() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+            string log_path = @"E:\\log.txt";
+            StreamWriter sw;
+            if (File.Exists(log_path)) {
+                sw = File.AppendText(log_path);
+            } else {
+                sw = File.CreateText(log_path);
+            }
+            sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] : Service Startup.");
+            sw.Close();
+        }
+
+        protected override void OnStop() {
+            string log_path = @"E:\\log.txt";
+            StreamWriter sw;
+            if (File.Exists(log_path)) {
+                sw = File.AppendText(log_path);
+            } else {
+                sw = File.CreateText(log_path);
+            }
+            sw.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] : Service Stop.");
+            sw.Close();
+        }
+    }
+}
---
+++ HippoKinectOverLoader/ServiceInstaller.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace HippoKinectOverLoader
+{
+    partial class ServiceInstaller
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HippoKinectProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.HippoKinectInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // HippoKinectProcessInstaller
+            // 
+            this.HippoKinectProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.HippoKinectProcessInstaller.Password = null;
+            this.HippoKinectProcessInstaller.Username = null;
+            // 
+            // HippoKinectInstaller
+            // 
+            this.HippoKinectInstaller.ServiceName = "HippoKinectOverloader";
+            // 
+            // ServiceInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.HippoKinectProcessInstaller,
+            this.HippoKinectInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller HippoKinectProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller HippoKinectInstaller;
+    }
+}
